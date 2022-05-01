5c81fc28069a522446daee28a499c786bc79bf3e
AAAG1824 <alonso.alvarado.gar@gmail.com>
2022-04-30 23:16:04 -0500

    Proyecto Base
    Vacio con Bunifu instalado

---
+++ ProyectoPOO/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub BunifuProgressBar1_ProgressChanged(sender As Object, e As Bunifu.UI.WinForms.BunifuProgressBar.ProgressChangedEventArgs)
+
+    End Sub
+End Class
